cffbef9ebc47df61ad75d7266116236dc132138e
felipesalmazo <felipesalmazo82@gmail.com>
2024-03-22 15:36:52 -0300

    feat: adicionando projeto ao repositório

---
+++ src/main/java/br/com/alura/techcase/AluraTechnicalCaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.techcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraTechnicalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraTechnicalCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/techcase/AluraTechnicalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.techcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraTechnicalCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
